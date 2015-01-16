c87aec5454e9a15c1c3de5b483a6daa647742a51
foysal <cfaion341@gmail.com>
2015-01-16 16:23:44 +0000

    added cors

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Soulmate
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete]
       end
     end
   end
